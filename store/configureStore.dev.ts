5fa5f17dcdc9e04fadbf1d6dfa4867f8fb0b1eca
Giovanny Gutiérrez <giovanny.gutierrez@merlinjobs.com>
2020-07-02 18:13:21 -0500

    feat: Add Redux devtools

--- store/configureStore.dev.ts
+++ store/configureStore.dev.ts
@@ -1,17 +1,30 @@
 import { RootState } from "../types/state";
 import { applyMiddleware, compose, createStore } from "redux";
-
+import { createLogger } from "redux-logger";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "../reducers";
 
 export default function configureStore(initialState: RootState) {
-  const sagaMiddleware = createSagaMiddleware();
+  const composeEnhancers =
+    (typeof window !== "undefined" &&
+      (<any>window).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
 
-  const store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(sagaMiddleware))
-  );
+  let store;
+  const sagaMiddleware = createSagaMiddleware();
+  if (typeof window !== "undefined" && (<any>window).devToolsExtension) {
+    store = createStore(
+      rootReducer,
+      initialState,
+      composeEnhancers(applyMiddleware(sagaMiddleware, createLogger()))
+    );
+  } else {
+    store = createStore(
+      rootReducer,
+      initialState,
+      composeEnhancers(applyMiddleware(sagaMiddleware))
+    );
+  }
 
   return { store, runSaga: sagaMiddleware.run };
 }
